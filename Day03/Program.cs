94a95e8f99f3a03fb91fb0ce9344b40b2108c49a
Matthew Toghill <matthew.toghill@outlook.com>
2024-12-03 09:03:35 +0000

    Add day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+public partial class Program
+{
+    private static readonly string _input = Input.ReadAll();
+    private static void Main()
+    {
+        Console.WriteLine($"Part 1: {Solve(Part1Regex())}");
+        Console.WriteLine($"Part 2: {Solve(Part2Regex())}");
+    }
+
+    private static int Solve(Regex regex)
+    {
+        bool isEnabled = true;
+        int result = 0;
+
+        foreach (Match item in regex.Matches(_input))
+        {
+            switch (item.Value[..3])
+            {
+                case "do(":
+                    isEnabled = true;
+                    break;
+                case "don":
+                    isEnabled = false;
+                    break;
+                case "mul":
+                    if (!isEnabled) continue;
+                    result += int.Parse(item.Groups[1].Value) * int.Parse(item.Groups[2].Value);
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex Part1Regex();
+
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")]
+    private static partial Regex Part2Regex();
+}
