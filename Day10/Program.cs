afbb3f557ca50ba3d59690e7ee694be79d899956
Matthew Toghill <matthew.toghill@outlook.com>
2024-12-10 17:03:33 +0000

    Update day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -7,18 +7,20 @@ public class Program
     private static readonly string[] _input = Input.ReadAllLines();
     private static readonly Position MinPos = new(0, 0);
     private static readonly Position MaxPos = new(_input[0].Length - 1, _input.Length - 1);
+
     private static void Main()
     {
-        Console.WriteLine($"Part 1: {Solve(true)}");
-        Console.WriteLine($"Part 2: {Solve(false)}");
+        var trailheads = FindTrailheads();
+        Console.WriteLine($"Part 1: {trailheads.Sum(x => x.Value.ToHashSet().Count)}");
+        Console.WriteLine($"Part 2: {trailheads.Sum(x => x.Value.Count)}");
     }
 
-    private static int Solve(bool uniqueTrailEnds)
+    private static Dictionary<Position, List<Position>> FindTrailheads()
     {
         Dictionary<Position, List<Position>> trailheads = [];
         _input.IterateGrid((row, col, c) =>
         {
-            if (c.ToInt() == 0)
+            if (c == '0')
             {
                 var start = new Position(col, row);
                 Queue<Position> queue = new([start]);
@@ -28,7 +30,7 @@ public class Program
                 {
                     var currentHeight = GetHeight(current);
 
-                    if (currentHeight == 9)
+                    if (currentHeight == '9')
                     {
                         trailheads[start].Add(current);
                         continue;
@@ -43,8 +45,8 @@ public class Program
             }
         });
 
-        return trailheads.Sum(x => uniqueTrailEnds ? x.Value.ToHashSet().Count : x.Value.Count);
+        return trailheads;
     }
 
-    private static int GetHeight(Position position) => _input[position.Row][position.Col].ToInt();
+    private static char GetHeight(Position position) => _input[position.Row][position.Col];
 }
