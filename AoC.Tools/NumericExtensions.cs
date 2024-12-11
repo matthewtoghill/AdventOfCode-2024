498fc56d13c75c079187a16cd66f8dbe3b222f5c
Matthew Toghill <matthew.toghill@outlook.com>
2024-12-11 09:37:39 +0000

    Add day 11

--- AoC.Tools/NumericExtensions.cs
+++ AoC.Tools/NumericExtensions.cs
@@ -7,6 +7,9 @@ public static class NumericExtensions
         return min.CompareTo(value) <= 0 && value.CompareTo(max) <= 0;
     }
 
+    public static int DigitCount(this int num) => (int)Math.Floor(Math.Log10(num) + 1);
+    public static int DigitCount(this long num) => (int)Math.Floor(Math.Log10(num) + 1);
+
     public static int Mod(this int num, int divisor) => ((num %= divisor) < 0) ? num + divisor : num;
     public static long Mod(this long num, long divisor) => ((num %= divisor) < 0) ? num + divisor : num;
 
---
+++ Day11/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Day11;
+
+public class Program
+{
+    private static readonly string _input = Input.ReadAll();
+    private static void Main()
+    {
+        Console.WriteLine($"Part 1: {Solve(25)}");
+        Console.WriteLine($"Part 2: {Solve(75)}");
+    }
+
+    private static long Solve(int iterations)
+    {
+        var stones = _input.ExtractNumeric<long>().GroupBy(x => x).ToDictionary(x => x.Key, x => (long)x.Count());
+
+        for (int i = 0; i < iterations; i++)
+        {
+            foreach (var (key, count) in stones.ToList())
+            {
+                stones.IncrementAt(key, -count);
+                if (stones[key] == 0) stones.Remove(key);
+
+                if (key == 0)
+                {
+                    stones.IncrementAt(1, count);
+                }
+                else
+                {
+                    var digits = key.DigitCount();
+                    if (digits % 2 == 0)
+                    {
+                        var (left, right) = SplitNumber(key, digits);
+                        stones.IncrementAt(left, count);
+                        stones.IncrementAt(right, count);
+                    }
+                    else
+                    {
+                        stones.IncrementAt(key * 2024, count);
+                    }
+                }
+            }
+        }
+
+        return stones.Values.Sum();
+    }
+
+    private static (long, long) SplitNumber(long num, int digits)
+    {
+        var divisor = (long)Math.Pow(10, digits / 2);
+        return (num / divisor, num % divisor);
+    }
+}
