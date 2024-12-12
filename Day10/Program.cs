12e1cd8a33890dba42cc7713aca5ebaaf88f4d0b
Matthew Toghill <matthew.toghill@outlook.com>
2024-12-12 12:29:13 +0000

    Update day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -5,48 +5,40 @@ namespace Day10;
 public class Program
 {
     private static readonly string[] _input = Input.ReadAllLines();
-    private static readonly Position MinPos = new(0, 0);
-    private static readonly Position MaxPos = new(_input[0].Length - 1, _input.Length - 1);
-
     private static void Main()
     {
-        var trailheads = FindTrailheads();
+        var trailheads = FindTrailheads(_input);
         Console.WriteLine($"Part 1: {trailheads.Sum(x => x.Value.ToHashSet().Count)}");
         Console.WriteLine($"Part 2: {trailheads.Sum(x => x.Value.Count)}");
     }
 
-    private static Dictionary<Position, List<Position>> FindTrailheads()
+    private static Dictionary<Position, List<Position>> FindTrailheads(string[] input)
     {
         Dictionary<Position, List<Position>> trailheads = [];
-        _input.IterateGrid((row, col, c) =>
+        var map = input.AsCharMap();
+
+        foreach (var (start, _) in map.Where(x => x.Value == '0'))
         {
-            if (c == '0')
+            Queue<Position> queue = new([start]);
+            trailheads[start] = [];
+
+            while (queue.TryDequeue(out var current))
             {
-                var start = new Position(col, row);
-                Queue<Position> queue = new([start]);
-                trailheads[start] = [];
-
-                while (queue.TryDequeue(out var current))
+                var height = map[current];
+                if (height == '9')
                 {
-                    var currentHeight = GetHeight(current);
+                    trailheads[start].Add(current);
+                    continue;
+                }
 
-                    if (currentHeight == '9')
-                    {
-                        trailheads[start].Add(current);
-                        continue;
-                    }
-
-                    current.GetNeighbours().ForEach(n =>
-                    {
-                        if (n.IsBetween(MinPos, MaxPos) && GetHeight(n) == (currentHeight + 1))
-                            queue.Enqueue(n);
-                    });
+                foreach (var neighbour in current.GetNeighbours())
+                {
+                    if (map.GetValueOrDefault(neighbour) == height + 1)
+                        queue.Enqueue(neighbour);
                 }
             }
-        });
+        }
 
         return trailheads;
     }
-
-    private static char GetHeight(Position position) => _input[position.Row][position.Col];
 }
