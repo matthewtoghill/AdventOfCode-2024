9d068aa687da6b7adaddce7693e21986ccdd4cea
Matthew Toghill <matthew.toghill@outlook.com>
2024-12-19 08:06:27 +0000

    Update day 19

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -5,25 +5,15 @@ public class Program
     private static readonly string[] _input = Input.ReadAsParagraphs();
     private static void Main()
     {
-        var possibleDesigns = Solve();
+        var possibleDesigns = Solve(_input[0].SplitOn(", "), _input[1].SplitLines());
         Console.WriteLine($"Part 1: {possibleDesigns.Count}");
         Console.WriteLine($"Part 2: {possibleDesigns.Values.Sum()}");
     }
 
-    private static Dictionary<string, long> Solve()
-    {
-        Dictionary<string, long> possibleDesigns = [];
-        var towels = _input[0].SplitOn(", ");
-
-        foreach (var design in _input[1].SplitLines())
-        {
-            var count = CalculatePossibleWays(design, towels, []);
-            if (count > 0)
-                possibleDesigns[design] = count;
-        }
-
-        return possibleDesigns;
-    }
+    private static Dictionary<string, long> Solve(string[] towels, string[] designs)
+        => designs.Select(x => new { Design = x, Count = CalculatePossibleWays(x, towels, []) })
+                  .Where(x => x.Count > 0)
+                  .ToDictionary(x => x.Design, x => x.Count);
 
     private static long CalculatePossibleWays(string design, string[] towels, Dictionary<string, long> cache)
     {
